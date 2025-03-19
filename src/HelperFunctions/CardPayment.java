f374e377600d4de16d182bbe946be7bed253e9b4
Keerthan <Deverakeerthan3030@gmail.com>
2025-03-18 22:23:39 -0400

    Card PaymentCommit

---
+++ src/HelperFunctions/CardPayment.java
@@ -0,0 +1,24 @@
+package HelperFunctions;
+
+
+public class CardPayment extends PaymentMethod {
+    public Card card;
+
+    public CardPayment(float amount , Card card) {
+        super(amount);
+        this.card = card;
+
+    }
+    @Override
+    public boolean processPayment(){
+        if(card.validateCard()){
+            return card.processPaymentAmount(amount);
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+}
