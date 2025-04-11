de62d9cb184679007b983c9dc75b7ffa297e8019
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-04-11 17:06:14 -0400

    Added randoop PasswordValidatorTest

---
+++ src/Test/Randoop/PasswordValidatorRegressionTest0.java
@@ -0,0 +1,48 @@
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PasswordValidatorRegressionTest0 {
+
+    public static boolean debug = false;
+
+    @Test
+    public void test1() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test1");
+        boolean boolean1 = HelperFunctions.PasswordValidator.isStrongPassword("hi!");
+        // Regression assertion (captures the current behavior of the code)
+        org.junit.Assert.assertTrue("'" + boolean1 + "' != '" + false + "'", boolean1 == false);
+    }
+
+    @Test
+    public void test2() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test2");
+        java.lang.Object obj0 = new java.lang.Object();
+        java.lang.Class<?> wildcardClass1 = obj0.getClass();
+        // Regression assertion (captures the current behavior of the code)
+        org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+
+    @Test
+    public void test3() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test3");
+        HelperFunctions.PasswordValidator passwordValidator0 = new HelperFunctions.PasswordValidator();
+        java.lang.Class<?> wildcardClass1 = passwordValidator0.getClass();
+        // Regression assertion (captures the current behavior of the code)
+        org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+
+    @Test
+    public void test4() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test4");
+        boolean boolean1 = HelperFunctions.PasswordValidator.isStrongPassword("");
+        // Regression assertion (captures the current behavior of the code)
+        org.junit.Assert.assertTrue("'" + boolean1 + "' != '" + false + "'", boolean1 == false);
+    }
+}
+
