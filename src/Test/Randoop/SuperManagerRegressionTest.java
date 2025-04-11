a2417f9868232b8c640597a824b2d466cb166c5a
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-04-11 16:37:23 -0400

    Added randoop SuperManagerTest

---
+++ src/Test/Randoop/SuperManagerRegressionTest.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ SuperManagerRegressionTest0.class })
+public class SuperManagerRegressionTest {
+}
+
