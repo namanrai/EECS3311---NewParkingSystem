8485d2e5e48d3aaca304b3bf9b9c41d4c8249a98
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-04-11 17:04:50 -0400

    Added randoop OccupiedStateTest

---
+++ src/Test/Randoop/OccupiedStateRegressionTest.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ OccupiedStateRegressionTest0.class })
+public class OccupiedStateRegressionTest {
+}
+
