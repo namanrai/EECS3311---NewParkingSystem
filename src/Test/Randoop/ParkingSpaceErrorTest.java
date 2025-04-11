db14858c4ff84bf0b53bc120d5759f2346208109
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-04-11 17:13:50 -0400

    Added randoop BookingTest

--- src/Test/Randoop/ParkingSpaceErrorTest.java
+++ src/Test/Randoop/ParkingSpaceErrorTest.java
@@ -1,10 +1,11 @@
 package Test.Randoop;
 
+import Models.ParkingSpace;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ErrorTest0.class })
+@Suite.SuiteClasses({ParkingSpaceErrorTest0.class })
 public class ParkingSpaceErrorTest {
 }
 
